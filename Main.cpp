8b9c401ec90fd9593f8a741aa987372238c83574
Akimori Ryuuji <ryuuji.Kun@icloud.com>
2015-12-21 20:39:23 -0500

    skeleton

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include "item.h"
+#include "entity.h"
+#include "player.h"
+#include "monster.h"
+#include "npc.h"
+#include "map.h"
+#include "quest.h"
+#include "magic.h"
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <cassert>
+#include <time.h>
+
+int main() {
+	gameOver = true;
+	while (!gameOver) {
+		break;
+	}
+	return 0;
+}
