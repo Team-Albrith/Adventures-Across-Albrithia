aaf05bb90974839d88b3e7913786f2372aab1dfb
Akimori Ryuuji <ryuuji.Kun@icloud.com>
2016-01-21 20:30:17 -0500

    Enchantment and Spell classes updated

--- Enchantment.hpp
+++ Enchantment.hpp
@@ -8,6 +8,23 @@ class Item;
 class Enchantment {
 public:
   Enchantment(); //!< Default Constructor
+  Enchantment(string newName, string newDescription);
+  //TODO: Constructors for skill, stat increase and effects
+
+  void setName(string newName);
+  void setDescription(string description);
+
+  //TODO: getter/setter for skills and effects
+
+private:
+    string name;
+    string description;
+
+    //Parallel array of skill and its increase
+    vector<int*> skill;
+    vector<int> statIncrease;
+
+    vector<Effect*> effects;
 };
 
 #endif
--- Spell.hpp
+++ Spell.hpp
@@ -7,19 +7,40 @@
 #include "Entity.hpp"
 
 using std::string;
+using std::vector;
+
+class Effect;
 
 //! Spell class
 /*! Derived from Magic; User-performed magics. */
 class Spell {
 public:
   Spell(); //!< Default Constructor
-};
+  Spell(string newName, string newDescription, vector<Effect*> newOnHitEffects);
+  Spell(string newName, string newDescription, int newManaCost, int newIntelligenceCost, vector<Effect*> newOnHitEffects);
+  ~Spell(); //!< Destructor
+  void PerformOn(const Entity* target); //!< Single-entity perform
+  void PerformOn(const vector<Entity*> targets); //! Multi-entity perform
 
-//! Buff class
-/*! Derived from Spell; Applies effects such as regen, poison, etc. */
-class Buff : public Spell {
-public:
-  Buff(); //!< Default Constructor
+  void setName(string newName);
+  void setDescription(string newDescription);
+  void getManaCost(int newManaCost);
+  void setRequiredIntelligence(int newRequiredIntelligence);
+  void setBaseDamage(int newBaseDamage);
+
+  string getName()const{return name;}
+  string getDescription()const{return description;}
+  int getManaCost()const{return manaCost;}
+  int getRequiredIntelligence()const{return requiredIntelligence;}
+  int getBaseDamage()const{return baseDamage;}
+  //TODO: Make getter/setter for onHitEffects
+private:
+    string name;
+    string description;
+    int manaCost;
+    int requiredIntelligence;
+    int baseDamage;
+    vector<Effect*> effects;
 };
 
 #endif
